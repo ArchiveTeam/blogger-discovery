b47867e82d5b75c39f6c8706c1c162cf8d3dda97
Arkiver2 <Arkiver@hotmail.com>
2015-02-26 15:23:17 +0100

    discover.py: remove sleep time

--- discover.py
+++ discover.py
@@ -75,7 +75,6 @@ def fetch(url):
 
     Returns True, returns the response text. Otherwise, returns None
     '''
-    time.sleep(1)
     print('Fetch', url)
     response = requests.get(url, headers=DEFAULT_HEADERS)
 
